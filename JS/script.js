80d69a260e8c942f49103a0ff7d994c4c6c2efae
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-07 15:56:16 +0200

    feat funzione per verificare se una parola è palindroma

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
 const parolautente = prompt("Inserisci una parola")
+
+function ispalindroma(parola){
+  const parolaminuscola = parola.toLowerCase();
+  const parolainvertita = parolaminuscola.split("").reverse().join("");
+  return parolaminuscola === parolainvertita;
+}
