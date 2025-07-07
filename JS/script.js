1ce9ec9dc0e288d357be8bbd0ec0cd3e950972c9
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-07 16:10:11 +0200

    feat inizio somma dei due numeri

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,17 @@ console.log(`La parola ${parolautente} non è palindroma`);
 
 const sceltautente = prompt("Segli: pari o dispari");
 
+const numeroutente = parseInt(prompt("inserisci un numero da 1 a 5"));
+
+if (numeroutente < 1 || numeroutente > 5 || isNaN(numeroutente)) {
+  console.log("Numero non valido, inserisci un numero da 1 a 5");
+} else {
+  function numerorandom() {
+   return Math.floor(Math.random() * 5) + 1;
+  }
+
+  const numerocomputer = numerorandom();
+  console.log(`La somma è ${somma}`);
+
+  
+}
